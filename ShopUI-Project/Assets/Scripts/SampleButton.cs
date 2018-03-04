07a2e21c687875e93d011ccc4a494ad57daa3004
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-03-03 18:28:37 -0800

    Arranged layout elements inside button to create a prefab button with the desired characteristics
    Based on the official tutorials from the Unity documentation. Link: https://unity3d.com/learn/tutorials/topics/user-interface-ui/shopscrolllist-script?playlist=17111

---
+++ ShopUI-Project/Assets/Scripts/SampleButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour {
+
+    public Button button;
+    public Text nameLabel;
+    public Text priceLabel;
+    public Image iconImage;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void SetUp()
+    {
+
+    }
+}
