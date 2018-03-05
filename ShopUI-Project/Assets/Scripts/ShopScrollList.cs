00396a4dc6c6c671d27cd9c6bea3b91e1448c0c8
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-03-05 02:35:31 -0800

    Added script for button pool and scroll list of items in interface.
    Based on the official tutorials from the Unity documentation. Link: https://unity3d.com/learn/tutorials/topics/user-interface-ui/shopscrolllist-script?playlist=17111

---
+++ ShopUI-Project/Assets/Scripts/ShopScrollList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable] // to set values of class in unity inspector
+public class Item
+{
+    public string itemName;
+    public Sprite icon;
+    public float price = 1f;
+}
+
+
+public class ShopScrollList : MonoBehaviour {
+
+    public List<Item> itemList;
+    public Transform contentPanel;
+    public ShopScrollList otherShop; // for panels talking to each other...
+    public Text myGoldDisplay;
+    public SimpleObjectPool buttonObjectPool;
+    public float gold = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    // adds buttons for each item stored in our item list.
+    // calls setup function of sample button
+    private void AddButtons()
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            Item item = itemList[i];
+            GameObject newButton = buttonObjectPool.GetObject();
+        }
+    }
+}
---
+++ ShopUI-Project/Assets/Scripts/SimpleObjectPool.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// A very simple object pooling class
+public class SimpleObjectPool : MonoBehaviour
+{
+    // the prefab that this object pool returns instances of
+    public GameObject prefab;
+    //collection of currently inactive instances of the prefab
+    private Stack<GameObject> inactiveInstances = new Stack<GameObject>();
+
+    // Returns an instance of the prefab when needed
+    public GameObject GetObject()
+    {
+        GameObject spawnedGameObject;
+
+        // if there is an inactive instance of the prefab ready to return, return it 
+        if (inactiveInstances.Count > 0)
+        {
+            // remove the instance from the collection of inactive instances
+            spawnedGameObject = inactiveInstances.Pop();
+        }
+        // otherwise, create a new instance; since it's necessary
+        else
+        {
+            spawnedGameObject = (GameObject)GameObject.Instantiate(prefab);
+
+            // add the PooledObject component to the prefab so we know it came from this pool
+            PooledObject pooledObject = spawnedGameObject.AddComponent<PooledObject>();
+            pooledObject.pool = this; // this pool...
+            // why would there be more than one pool?
+        }
+
+        // put the instance in the root of the scene and enable it
+        spawnedGameObject.transform.SetParent(null); // so Parent null follows using default at origin...
+        spawnedGameObject.SetActive(true);
+
+        // return a reference to the instance
+        return spawnedGameObject;
+
+    }
+
+    // Return an instance of the prefab to the pool
+    public void ReturnObject(GameObject toReturn)
+    {
+        PooledObject pooledObject = toReturn.GetComponent<PooledObject>(); //which pool it comes from...
+
+        // if the instance came from this pool, return it to the pool
+        if (pooledObject != null && pooledObject.pool == this)
+        {
+            // make the instance a child of this and disable it
+            toReturn.transform.SetParent(transform); // didn't get this line...
+            toReturn.SetActive(false);
+        }
+        // otherwise, just destroy it
+        else
+        {
+            // WTF!!! Wouldn't a pool following a singleton pattern be better then???
+            Debug.LogWarning(toReturn.name + " was returned to a pool it wasn't spawned from!!! Destroying...");
+            Destroy(toReturn);
+        }
+    }
+}
+
+// a component that simply identifies the pool that a GameObject came from
+public class PooledObject : MonoBehaviour
+{
+    public SimpleObjectPool pool; //weird patterns...
+}
+
